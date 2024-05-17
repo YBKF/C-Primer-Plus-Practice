bd0eee0b922f64e8e913a78189451b5c7485a35f
Tee Yuan <yuantee1990@outlook.com>
2024-05-17 11:51:13 +0800

    12_9_7

---
+++ 12/12_9/12_9_7/12_9_7diceroll.h
@@ -0,0 +1,7 @@
+// diceroll.h
+#ifndef _INC_DICEROLL
+#define _INC_DICEROLL
+
+int roll_n_dice(int dice, int sides);
+
+#endif
--- 12/12_9/12_9_7/12_9_7manydice.c
+++ 12/12_9/12_9_7/12_9_7manydice.c
@@ -13,49 +13,114 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "diceroll.h"
 
-int main(int argc, char const *argv[])
+int main(void)
 {
-    int iDiceNum;   // Number of dice.
-    int iSidesNum;  // Number of sides of the dice.
-    int iRollTimes; // Number of times the dice were rolled.
-    int iStatus;    // Store scanf() return value.
+    int dice;            // 骰子的数量
+    int sides;           // 骰子的边数
+    int rollTimes;       // 投掷骰子的次数
+    int rollTimesStatus; // “状态值”，存储投掷骰子次数 rollTimes scanf() 的返回值
+    int diceStatus;      // “状态值”，存储骰子属性信息 sidesNum 和 diceNum scanf() 的返回值
+    int *diceResults;    // 存储投掷骰子的结果
 
-    srand((unsigned)time(NULL)); // Random seed.
+    srand((unsigned int)time(0)); /* 随机种子 */
 
-    fputs("Enter the number of sets; enter q to stop :\n", stdout);
+    printf("Enter the number of sets; enter q to stop :\n");
 
-    // If not EOF, then enter the while loop.
-    while ((iStatus = scanf("%d", &iRollTimes)) == 1)
+    while ((rollTimesStatus = scanf("%d", &rollTimes)) && rollTimes > 0)
     {
-        // Check iRollTimes.
-        if (iRollTimes < 1)
-        {
-            fputs("Please enter a number, or the number of sets you entered must be bigger than 0, please input again.\n", stdout);
+        printf("rollTimes: %d\n", rollTimes);
 
-            // Clean
+        /* 循环内的输入返回值检测代码块 start */
+        if (rollTimesStatus == 1) // 读入成功时
+        {
+            while (getchar() != '\n')
+                continue;
+        }
+        else if (rollTimesStatus == EOF) // 读入时遭遇错误（类型不匹配，通常是输入了字符）或EOF时
+        {
+            /* 错误输入流中获取到第一个字符为q则退出循环，不为q则打印错误信息并重新输入 */
+            if (getchar() != 'q')
+            {
+                while (getchar() != '\n')
+                    continue;
+                break;
+            }
+            else
+            {
+                fputs("Cannot match this parameter.\nPlease enter a number to continue, or 'q' to stop:\n",
+                      stdout);
+                while (getchar() != '\n')
+                    continue;
+
+                continue;
+            }
+        }
+        else if (rollTimesStatus == 0) // rollTimes 值读入失败时
+        {
+            /* 打印错误信息并重新输入 */
+            fputs("Fail to get a value of \"rollTimes\".\nPlease enter a number to continue, or 'q' to stop:\n",
+                  stdout);
             while (getchar() != '\n')
                 continue;
 
-            fputs("Please enter the number of sets; enter q to stop :\n", stdout);
+            continue;
+        }
+        else // 其他原因的不合法输入
+        {
+            fputs("Invalid input.\nPlease enter a number to continue, or 'q' to stop :\n",
+                  stdout);
+            while (getchar() != '\n')
+                continue;
 
             continue;
         }
+        /* 循环内的输入返回值检测代码块 end */
 
-        fputs("How many sides and how many dice? ", stdout);
+        printf("How many sides and how many dice?\n");
 
-        while ((iStatus = scanf("%d %d", &iSidesNum, &iDiceNum)) == 2)
+        /* 此处的代码尽量与原代码的逻辑保持一致 */
+        if ((diceStatus = scanf("%d %d", &sides, &dice)) != 2)
         {
+            if (diceStatus == EOF)
+                break; /* 退出循环 */
+            else
+            {
+                printf("You should have entered two integer.");
+                printf(" Let's begin again.\n");
+
+                while (getchar() != '\n')
+                    continue; /* 处理错误的输入 */
+
+                printf("How many sets? Enter q to stop :\n");
+
+                continue; /* 进入循环的下一轮迭代 */
+            }
         }
 
-        if (scanf("%d %d", &iSidesNum, &iDiceNum) == 2)
+        diceResults = (int *)malloc(rollTimes * sizeof(int));
+        if (diceResults == NULL)
         {
+            fputs("Error: malloc failed.\n", stderr);
+            exit(EXIT_FAILURE);
         }
-        else
-        {
-            fputs("Please enter two correct numbers.\n", stdout);
-        }
+
+        for (int i = 0; i < rollTimes; i++)
+            diceResults[i] = roll_n_dice(dice, sides);
+
+        printf("Here are %d sets of %d %d-sided throws.\n",
+               rollTimes, dice, sides);
+
+        for (int i = 0; i < rollTimes; i++)
+            printf("%d ", diceResults[i]);
+        putchar('\n');
+        free(diceResults);
+
+        printf("How many sets? Enter q to stop :\n");
     }
+
+    /* TODO: Do something, when rollTimes < 0. */
 
     return 0;
 }
