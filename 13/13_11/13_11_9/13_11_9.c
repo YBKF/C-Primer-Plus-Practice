73239ee1712e9355a7a732e9772bb0087e1b6e9c
Tee Yuan <yuantee1990@outlook.com>
2024-06-13 01:10:56 +0800

    Update 13_11_9.c

--- 13/13_11/13_11_9/13_11_9.c
+++ 13/13_11/13_11_9/13_11_9.c
@@ -10,34 +10,42 @@
 #include <string.h>
 
 #define MAX 41
+#define BUF_SIZE (4096)
 
 /**
  * 文件中文本的合法格式：
  * [文件开始] + [一行(一个单词)] + [下一行(下一个单词)] + ... + [文件结束]
  *
  * 文件中“一行（一个单词）”合法格式：
- * [数字（编号）] + [一个制表符\t] + [一串字符串(中间不允许有空格)] + [一个换行符\n]
+ * [数字（编号）] + [空白字符] + [一串字符串(中间不允许有空格)] + [一个换行符\n]
  *
- * 程序打开文件后，
- * 首先读取并验证文件第一行的内容的格式是否合法并且是否为空
- *      不合法，则继续检查文件是否为空
- *          不为空，则报告错误、关闭文件并退出程序
- *          为空，则准备进行单词输入
- *      如果格式合法，则继续验证本行编号是否合法（是否为1），
- *
- *          如果本行编号为1，则继续
- *
- * BEGIN:   程序打开文件，
- * *1:  首先读取并验证文件第一行的内容的格式是否合法并且是否为空
+ * STATIC:  number = 0; 存储程序运行时检测到的最新的合法编号
+ * BEGIN:   程序以读写模式打开文件，
+ * *1:  读取并验证文件[第一行]的内容的格式是否合法
  * *a1:     格式合法，则继续验证本行编号是否合法
- * *aa1:    
- * *
+ * *aa1:        编号合法（检测是否为 number + 1），number = 当前编号
+ * *aa2:            然后，goto 1，验证下一行
+ * *ab1:        编号不合法（!= number + 1），则报告错误、关闭文件并退出程序
+ * *b1:     格式不合法，则继续检查本行是否为空
+ * *ba1:        为空，则准备进行单词输入
+ * *bb1:        不为空，则报告错误、关闭文件并退出程序
  */
+
+int check_file_line(FILE *file);
+int check_file(FILE *file);
+
 int main(void)
 {
     FILE *fp;
     char words[MAX];
 
+    if ((fp = fopen("wordy", "r+")) == NULL)
+    {
+        fprintf(stdout, "Can't open \"wordy\" file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fclose(fp);
     if ((fp = fopen("wordy", "a+")) == NULL)
     {
         fprintf(stdout, "Can't open \"wordy\" file.\n");
@@ -62,4 +70,90 @@ int main(void)
         fprintf(stderr, "Error closing file\n");
 
     return 0;
+}
+
+/**
+ * 检查文件中一行格式的合法性，
+ * 返回值：
+ * > 0  ，此行合法，返回值为此行的编号；
+ * EOF  ，遇到文件结尾；
+ * 0    ，遇到其他错误。
+ *
+ * 文件中“一行（一个单词）”合法格式：
+ * [数字（编号）] + [空白字符（空格）] + [一串字符串（中间不允许有空格）] + [一个换行符\n]
+ */
+int check_file_line(FILE *file)
+{
+    if (file == NULL)
+        return 0;
+
+    int num_line;
+    char word_temp;
+    int status; // 用于获取fscanf()函数的返回值，当在函数中检测到文件的不合规的错误时便于进行分析处理
+
+    /*
+    一行的合法性检测分为两步，
+    1、使用fscanf()函数检测换行符之前的格式的合法性；
+    2、检测第一步之后的连续的字符是否为换行符，如果为换行符则合法。
+    在此处先进行第一步检测，第一步通过后再进行第二步检测。
+
+    fscanf() 返回值为 2 且第二步检测到换行符时，则表示本行合法（由于此处简单使用了fscanf(file, " %d %s\n", &num_line, word_temp)，所以并不保证严格合法）
+    fscanf() 返回值为 -1 时，则表示顺利检测到文件结尾；
+    其他返回值都表示本行不合法，需要在调用函数处报告错误。 */
+    if ((status = fscanf(file, " %d %s\n", &num_line, word_temp)) == 2)
+    {
+        fseek(file, -1L, SEEK_CUR);
+        if (getc(file) != '\n') // 未检测到换行符，本行不合法，返回0
+            return 0;
+        else
+            return num_line;
+    }
+    else if (status == EOF && !ferror(file))
+        return EOF;
+    else
+        return 0;
+}
+
+/**
+ * 检查文件合法性并返回文件中最后一个单词的编号，
+ * 若文件不合法则返回0
+ *
+ * 文件中“一行（一个单词）”合法格式：
+ * [数字（编号）] + [空白字符（空格）] + [一串字符串（中间不允许有空格）] + [一个换行符\n]
+ */
+int check_file(FILE *file)
+{
+    if (file == NULL)
+        return EOF;
+
+    static int word_number = 0; // 单词的编号，文件合法时将存储文件最后一个单词的编号，也用于检测单词编号是否合法
+    int number_temp;
+    int status;
+
+    // 检查每一行的内容的合法性
+    while ((status = check_file_line(file)) > 0)
+    {
+        // 进入循环的 status 值为当前行的编号
+        number_temp = status;
+
+        // 格式检测通过，然后检查编号合法性
+        if (number_temp == (word_number + 1))
+            word_number = number_temp;
+        else
+        {
+            status = 0;
+            fprintf(stderr, "ERROR: Invalid word number: %d\n", number_temp);
+            break;
+        }
+    }
+
+    if (status == EOF && !ferror(file))
+    {
+        return word_number;
+    }
+    else // status == 0
+    {
+        fputs("ERROR: Invalid file formation.\n", stderr);
+        return 0;
+    }
 }
