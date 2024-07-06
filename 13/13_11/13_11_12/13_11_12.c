09d57f6e7e8737416a7b45214529090bf6ef6e76
Tee Yuan <yuantee1990@outlook.com>
2024-07-06 23:59:19 +0800

    Update 13_11_12.c

--- 13/13_11/13_11_12/13_11_12.c
+++ 13/13_11/13_11_12/13_11_12.c
@@ -9,42 +9,91 @@
  * 每行的最后一个字符（第31个）是空字符，这样该数组包含了20个字符串。
  * 最后，程序显示最终的图片（即，打印所有的字符串），并将结果储存在文本文件中。
  */
-// TODO: 可有malloc()动态数组版本
 
+/**
+ * 首先，读取文件中的一行，尝试将此行的所有数字字符转换为整型，读到单行字符上限（30）为止，重复调用至要求的次数（20）；
+ */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-#define INT_PIC_LENGTH (20)
-#define INT_PIC_WIDTH (30)
-#define CHAR_PIC_LENGTH_SIZE (INT_PIC_LENGTH + 1)
+#define INT_PIC_LENGTH (30)
+#define INT_PIC_WIDTH (20)
+#define CHAR_PIC_LENGTH INT_PIC_LENGTH
+#define CHAR_PIC_LENGTH_SIZE (CHAR_PIC_LENGTH + 1)
 #define CHAR_PIC_WIDTH INT_PIC_WIDTH
+#define CHAR_PIC_PIXEL_SIZE (4)
 
-int **checkAndGetPic(int picLength, int picWidth, int (*iPic)[INT_PIC_LENGTH], FILE *file);
-void transFile2IntPic(FILE *file, int (*iPic)[INT_PIC_LENGTH]);
-void transIntPic2CharPic(int (*iPic)[INT_PIC_LENGTH], char (*chPic)[CHAR_PIC_LENGTH_SIZE]);
+int parseCharPicture2Int(int iPicLength, int iPicWidth, int iPic[][INT_PIC_LENGTH], FILE *file);
 
+/**
+ * 源文件：字符形式存在的数字
+ * 数字图：纯整型
+ * 字符图：（数字转换为字符后的）字符
+ */
 int main(int argc, char const *argv[])
 {
+    int iPic[INT_PIC_WIDTH][INT_PIC_LENGTH] = {};
+
+    parseCharPicture2Int(INT_PIC_LENGTH, INT_PIC_WIDTH, iPic, stdin);
+
     return 0;
 }
 
-
 /**
- * 检查源文件中每行的字符数量是否符合用户对图像长度的要求(?==picLength)，
- * 一行中读取的字符的数量不超过要求的长度(picLength);
- * 一行中字符的数量未到达要求的长度(picLength)的，
+ * 检查源文件中每行的字符数量是否符合用户对图像长度的要求(?==iPicLength)，
+ * 一行中读取的字符的数量不超过要求的长度(iPicLength);
+ * 一行中字符的数量未到达要求的长度(iPicLength)的，
  * 则在存储到图像(iPic)中时会将剩下的空位填充为数字0，
  * 一直读到文件结尾为止;
  *
- * 检查源文件读取的行的数量是否符合用户对图像宽度的要求(?==picWidth)，
- * 最多读取到要求的行的数量(picWidth);
- * 若读完整个文件时，读取到的行的行数低于要求的行数(picWidth)，
+ * 检查源文件读取的行的数量是否符合用户对图像宽度的要求(?==iPicWidth)，
+ * 最多读取到要求的行的数量(iPicWidth);
+ * 若读完整个文件时，读取到的行的行数低于要求的行数(iPicWidth)，
  * 则读取到的行数到要求行数之间的所有空行均被填充为数字0。
  *
  * 以上过程中读取到的内容，均保存到用户指定的图像（二维整型数组iPic）中;
- * 读取完毕后将返回第三个参数地址，
- * 读取过程中遇到任何错误则返回空指针(NULL)
+ * 读取完毕后将返回成功读取到的元素的数量，
+ * 读取过程中遇到任何错误则报告错误并返回当前已读取到的值
  */
-int **checkAndGetPic(int picLength, int picWidth, int iPic[][], FILE *file)
+int parseCharPicture2Int(int iPicLength, int iPicWidth, int iPic[][INT_PIC_LENGTH], FILE *file)
 {
+    if (file == NULL)
+    {
+        fputs("ERROR: NULL File.\n", stderr);
+        return 0;
+    }
+
+    if (iPic == NULL)
+    {
+        fputs("ERROR: NULL Pointer.\n", stderr);
+        return 0;
+    }
+
+    if (iPicLength <= 0 || iPicWidth <= 0)
+        return 0;
+
+    /**
+     * *1:  首先，检测 [本行已保存的字符串的数量] 是否小于要求的长度(iPicLength)；
+     * *a1:     小于要求的长度(iPicLength)，则从文件中读取一个字符并把这个字符存储到一个临时的 [字符缓存] 中，
+     * *a2:     然后，检测 [字符缓存] 中的字符是否为数字字符；
+     * *aa1:        为数字字符，则将 [字符缓存] 中的字符存储到 [字符串缓存] 数组中（此数组只有4个元素，最多存储3个有效字符），\
+     * *aa2:        然后，goto a1；
+     * *ab1:        不为数字字符，则检测此字符:
+     * *aba1:           - 为空格字符，则将 [字符串缓存] 中的字符转换为整型，并保存到 [二维整型数组] 中 [二维整型数组] 列数+1，[本行已保存的字符串的数量] + 1，[以保存的字符串的总量] + 1，\
+     * *aba2:           然后 goto 1；
+     * *abb1:           - 为换行符，则将 [字符串缓存] 中的字符转换为整型，并保存到 [二维整型数组] 中，\
+     * *abb2:           然后，检测 [本行已保存的字符串的数量] 是否小于要求的长度(iPicLength)；
+     * *abba1:              小于要求的长度，则将 [二维整型数组] 当前行剩下的所有元素填充为 0，\
+     * *abba2:              然后，goto 1；
+     * *abbb1:              不小于要求的长度，则 goto 1；
+     * *abc1:           - 为文件结尾(EOF)，则 [文件结尾标志] 设为 true，
+     * *abc2:           然后，若 [本行已保存的字符串的数量] 小于要求的长度(iPicLength)，则将 [二维整型数组] 当前行剩下的所有元素填充为 0
+     * *abc3:           最后，goto 1；
+     * *abd1:           - 为其他字符，则抛弃此字符，goto a1；
+     * *b1:     不小于要求的长度(iPicLength)，检测是否到达文件结尾（检测 [文件结尾标志] 是否为 true）；
+     * *ba1:        为true，则返回 [以保存的字符串的总量]；
+     * *bb1:        不为true，则 [二维整型数组] 行数+1， [本行已保存的字符串的数量] 置零
+     */
 }
