f2dfe4fbe4ede4f9f468df3540ebd2492e3d7f4b
Tee Yuan <yuantee1990@outlook.com>
2024-08-15 00:09:10 +0800

    Update 14_18_7.c

--- 14/14_18/14_18_7/14_18_7.c
+++ 14/14_18/14_18_7/14_18_7.c
@@ -1,11 +1,10 @@
 /**
  * 7.修改程序清单 14.14，
- * 从文件中读取每条记录并显示出来，允许用户删除记录或修改记录的内容。
+ * 从文件中读取每条记录并显示出来，允许用户 删除记录 或 修改记录 的内容。
  * 如果删除记录，把空出来的空间留给下一个要读入的记录。
  * 要修改现有的文件内容，必须用"r+b"模式，而不是"a+b"模式。
  * 而且，必须更加注意定位文件指针，防止新加入的记录覆盖现有记录。
- * 最简单的方法是改动储存在内存中的所有数据，
- * 然后再把最后的信息写入文件。
+ * 最简单的方法是改动储存在内存中的所有数据，然后再把最后的信息写入文件。
  * 跟踪的一个方法是在book结构中添加一个成员表示是否该项被删除。
  */
 #include <stdio.h>
@@ -32,20 +31,19 @@ int main(void)
     FILE *pbooks;
     int size = sizeof(struct book);
 
-    if ((pbooks = fopen("book.dat", "a+b")) == NULL)
+    if ((pbooks = fopen("book.dat", "r+b")) == NULL)
     {
         fputs("Can't open book.dat file\n", stderr);
         exit(1);
     }
 
     rewind(pbooks); /* 定位到文件开始 */
-    while (count < MAXBKS && fread(&library[count], size,
-                                   1, pbooks) == 1)
+    while (count < MAXBKS && fread(&library[count], size, 1, pbooks) == 1)
     {
         if (count == 0)
             puts("Current contents of book.dat:");
-        printf("%s by %s: $%.2f\n", library[count].title,
-               library[count].author, library[count].value);
+        printf("%s by %s: $%.2f\n",
+               library[count].title, library[count].author, library[count].value);
         count++;
     }
 
@@ -59,7 +57,6 @@ int main(void)
 
     puts("Please add new book titles.");
     puts("Press [enter] at the start of a line to stop.");
-
     while (count < MAXBKS && s_gets(library[count].title, MAXTITL) != NULL && library[count].title[0] != '\0')
     {
         puts("Now enter the author.");
@@ -76,10 +73,9 @@ int main(void)
     {
         puts("Here is the list of your books:");
         for (index = 0; index < count; index++)
-            printf("%s by %s: $%.2f\n", library[index].title,
-                   library[index].author, library[index].value);
-        fwrite(&library[filecount], size, count - filecount,
-               pbooks);
+            printf("%s by %s: $%.2f\n",
+                   library[index].title, library[index].author, library[index].value);
+        fwrite(&library[filecount], size, count - filecount, pbooks);
     }
     else
         puts("No books? Too bad.\n");
