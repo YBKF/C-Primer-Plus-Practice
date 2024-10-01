9a5c48b4388ce6277e9684f67174a30aa96f6513
Tee Yuan <yuantee1990@outlook.com>
2024-10-01 23:49:07 +0800

    Update 14_18_8.c

--- 14/14_18/14_18_8/14_18_8.c
+++ 14/14_18/14_18_8/14_18_8.c
@@ -1,7 +1,7 @@
 /**
  * 8.巨人航空公司的机群由 2 个座位的飞机组成。它每天飞行一个航班。
  * 根据下面的要求，编写一个座位预订程序。
- * 
+ *
  * a.该程序使用一个内含 12 个结构的数组。每个结构中包括：一个成员表示座位编号、一个成员表示座位是否已被预订、一个成员表示预订人的名、一个成员表示预订人的姓。
  * b.该程序显示下面的菜单：
  * To choose a function, enter its letter label:
@@ -17,8 +17,22 @@
 
 #include <stdio.h>
 
+void showMenu();
+
 int main(int argc, char const *argv[])
 {
 
     return 0;
+}
+
+void showMenu()
+{
+    fprintf(stdout, "\
+To choose a function, enter its letter label:\n\
+  a) Show number of empty seats\n\
+  b) Show list of empty seats\n\
+  c) Show alphabetical list of seats\n\
+  d) Assign a customer to a seat assignment\n\
+  e) Delete a seat assignment\n\
+  f) Quit\n");
 }
