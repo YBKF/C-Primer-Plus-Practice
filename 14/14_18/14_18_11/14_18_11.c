e257f2344f11dfccf5e2dbe81a6fff93f66a17f1
Tee Yuan <yuantee1990@outlook.com>
2025-10-18 16:33:10 +0800

    Create 14_18_11.c

---
+++ 14/14_18/14_18_11/14_18_11.c
@@ -0,0 +1,17 @@
+/**
+ * 11.编写一个名为 transform() 的函数，接受4个参数：
+ * 内含 double 类型数据的源数组名、内含 double 类型数据的目标数组名、一个表示数组元素个数的 int 类型参数、函数名（或等价的函数指针）。
+ * transform() 函数应把指定函数应用于源数组中的每个元素，并把返回值储存在目标数组中。
+ * 例如：transform(source, target, 100, sin);
+ * 该声明会把 target[0] 设置为 sin(source[0])，等等，共有100个元素。
+ * 在一个程序中调用 transform() 4次，以测试该函数。
+ * 分别使用 math.h 函数库中的两个函数以及自定义的两个函数作为参数。
+ */
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+    return 0;
+}
